a2b81dd54bc3ea07410512d645fab49db934291b
frank <paintersmechanic@gmail.com>
2020-03-30 14:17:20 -0700

    second commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from motor import motor_asyncio
 
 env_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path=env_path)
-SIGNING_SECRET = os.environ.get('v_TOKEN')
+SIGNING_SECRET = os.environ.get('SECRET_KEY')
 app = Sanic(__name__)
 client = motor_asyncio.AsyncIOMotorClient(os.environ.get("MONGODB_URI"))
 db = client[os.environ.get("MONGODB_DATABASE")]
@@ -17,21 +17,22 @@ collection = db[os.environ.get("MONGODB_COLLECTION")]
 
 @app.route('/pray', methods=['POST'])
 async def pray(request):
-  token = request.form['token']
-  command = request.form['command']
-  res_text = request.form['text']
-  if token != SIGNING_SECRET:
-    return response.HTTPResponse(status=400)
-  prayer = {
-    "user_id":request.form['user_id'],
-    "when":datetime.utcnow(),
-    "text":res_text
-  }
-  await prayer.commit()
-  await json.dumps(prayer)
+  # response = request.form
+  # command = response['command']
+  # res_text = response['text']
+  # if request.form['token'] != SIGNING_SECRET:
+  #   return response.HTTPResponse(status=400)
+  # prayer = {
+  #   "user_id":request.form['user_id'],
+  #   "when":datetime.utcnow(),
+  #   "text":res_text
+  # }
+  # await prayer.commit()
+  # await json.dumps(prayer)
+  response_url = request.form.get('response_url')
   return json({
     "response_type":'in_channel',
-    "text": "prayer added"}),
+    "text": "prayer added"},status=200, content_type="application/json"),
   
 
 
