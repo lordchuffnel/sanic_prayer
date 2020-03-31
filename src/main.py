4c90721fbbf886e09f5c6cfd541f8b7cf2b7cb54
Frank Huff <paintersmechanic@gmail.com>
2020-03-30 17:14:09 -0700

    adding event loop

--- src/main.py
+++ src/main.py
@@ -1,40 +1,35 @@
 import os
+from asyncio.events import get_event_loop
+from asyncio.tasks import ensure_future
 from datetime import datetime
 from dotenv import load_dotenv
 from sanic import Sanic, response
-from sanic.response import json
-from motor import motor_asyncio
+from sanic.response import dumps, json
+from pymongo import MongoClient
 
 env_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path=env_path)
-SIGNING_SECRET = os.environ.get('SECRET_KEY')
+client = MongoClient(os.environ.get('MONGODB_URI'))
+db = client.ccprayer
 app = Sanic(__name__)
-client = motor_asyncio.AsyncIOMotorClient(os.environ.get("MONGODB_URI"))
-db = client[os.environ.get("MONGODB_DATABASE")]
-collection = db[os.environ.get("MONGODB_COLLECTION")]
 
 
+async def respond():
+  return json({
+    "response_type":'in_channel',
+    "text": "Humans require ice cream"})
 
 @app.route('/pray', methods=['POST'])
 async def pray(request):
-  # response = request.form
-  # command = response['command']
-  # res_text = response['text']
-  # if request.form['token'] != SIGNING_SECRET:
-  #   return response.HTTPResponse(status=400)
-  # prayer = {
-  #   "user_id":request.form['user_id'],
-  #   "when":datetime.utcnow(),
-  #   "text":res_text
-  # }
-  # await prayer.commit()
-  # await json.dumps(prayer)
-  response_url = request.form.get('response_url')
-  return json({
-    "response_type":'in_channel',
-    "text": "prayer added"},status=200, content_type="application/json"),
+  if not res['token']:
+    return response.HTTPResponse(status=400)
+  prayer = db.prayer.insert_one({
+    "user_id":request.form['user_id'],
+    "when":datetime.utcnow(),
+    "text": request.form['text']
+  })
+  
   
 
-
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  asyncio.run()
